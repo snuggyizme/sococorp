dcf92661ebdfa8a402004987e4c42604a6488d5c
snuggyizme <snuggyizme@gmail.com>
2025-11-25 20:35:57 +1100

    25-11-25 : level structure, object space

--- scripts/ObjectGrid.gd
+++ scripts/ObjectGrid.gd
@@ -6,6 +6,7 @@ const objectGridWidth = 10
 const objectGridHeight = 10
 
 var objectGrid = []
+var objectGridQueue = []
 var loadedMap = GlobalUtilities.loadJson("res://data/levels/prototyping/test01.json")
 
 func _ready():
@@ -27,8 +28,18 @@ func _ready():
 			for i in loadedMap["preplacedBuildings"]:
 				if i["x"] == x and i["y"] == y:
 					appendage = i["object"]
-			
+					objectGridQueue += calculateOccupatedSpaces(i["id"])
+				elif [i["x"], i["y"]] in objectGridQueue:
+					appendage = grid.occupied
 			
 			row.append(appendage)
 		objectGrid.append(row)
 	print(objectGrid)
+
+func calculateOccupatedSpaces(targetId):
+	var targetObject = GlobalUtilities.getObject(targetId)
+	var export = []
+	for i in targetObject["x"]:
+		for u in targetObject["y"]:
+			export.append([i, u])
+	return export
